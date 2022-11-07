1ff1a50e6d11fbe7e966ec663209c87cc9be3a8a
BhargavMN <bhargav.malasaninagaraj@student.put.poznan.pl>
2022-11-07 23:57:57 +0100

    Result+Winner
    Implementation of result and winner functions

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,7 @@
 """
 Tic Tac Toe Player
 """
-
+import copy
 import math
 
 X = "X"
@@ -33,10 +33,10 @@ def player(board):
                 counter_o += 1
 
     return X if counter_x == counter_o else O
-    raise NotImplementedError
 
 
 def actions(board):
+
     """
     Returns set of all possible actions (i, j) available on the board.
     """
@@ -48,7 +48,6 @@ def actions(board):
                 action_set.add((row, coll))
 
     return action_set
-    raise NotImplementedError
 
 
 def result(board, action):
@@ -56,19 +55,43 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     """
 
+    i, j = action
+
+    if i not in range(3) or j not in range(3):
+        raise ValueError('Invalid Co-ordinates')
+    if board[i][j] != EMPTY:
+        raise ValueError('Non-empty Co-ordinates')
+    board_copy = copy.deepcopy(board)
+    board_copy[i][j] = player(board)
+
+    return board_copy
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for i in range(3):
+        if board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][2] != EMPTY:
+            return board[i][0]
+        if board[0][i] == board[1][i] and board[1][i] == board[2][i] and board[2][i] != EMPTY:
+            return board[0][i]
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[2][2] != EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[2][0] != EMPTY:
+        return board[0][0]
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
+    for row in range(3):
+        for coll in range(3):
+            if board[row][coll] == EMPTY:
+                return
+
     raise NotImplementedError
 
 
